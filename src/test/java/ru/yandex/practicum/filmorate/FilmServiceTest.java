5fb17820a6a18590644feb7a276aeac425fcc5a5
Dmitry Kuznetsov <dimakuznetcov163@yandex.ru>
2024-06-19 00:00:05 +0400

    Реализация функциональности по тз спринта 11(новое)

--- src/test/java/ru/yandex/practicum/filmorate/FilmServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmServiceTest.java
@@ -9,7 +9,6 @@ import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
