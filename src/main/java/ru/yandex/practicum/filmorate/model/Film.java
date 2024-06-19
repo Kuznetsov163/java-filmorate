a9dab5777adf459231db0d920a964dc6ccf209f8
Dmitry Kuznetsov <dimakuznetcov163@yandex.ru>
2024-06-19 16:01:51 +0400

    Реализация функциональности по тз спринта 11(новое)

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -8,10 +8,6 @@ package ru.yandex.practicum.filmorate.model;
 продолжительность фильма — duration.
  */
 import lombok.Data;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -21,19 +17,16 @@ import java.time.LocalDate;
   @Data
 
   public class Film {
+
     private Integer id;
 
-  @NotBlank(message = "Название фильма не может быть пустым")
   private String name;
 
-  @Size(max = 200, message = "Максимальная длина описания — 200 символов")
   private String description;
 
-  @NotNull(message = "Дата релиза не может быть пустой")
 
   private LocalDate releaseDate;
 
-  @Positive(message = "Продолжительность фильма должна быть положительным числом")
   private int duration;
 
   private Set<Integer> likes = new HashSet<>();
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -9,9 +9,6 @@ package ru.yandex.practicum.filmorate.model;
  */
 
 import lombok.Data;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Past;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
@@ -22,16 +19,15 @@ public class User {
 
      private Integer id;
 
-    @NotBlank(message = "Электронная почта не может быть пустой!")
-    @Email(message = "Некорректная электронная почта!")
+
     private String email;
 
-    @NotBlank(message = "Логин не может быть пустым")
+
     private String login;
 
     private String name;
 
-    @Past(message = "Дата рождения не может быть в будущем")
+
     private LocalDate birthday;
 
     private Set<Integer> friends = new HashSet<>();
